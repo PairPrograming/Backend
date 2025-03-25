a90b90a8eb778c73fff43c7b2a31d47708f4401d
Slasho01 <josuerendon01@gmail.com>
2025-03-24 23:54:16 -0300

    Evento handler

--- src/Handlers/EventoHandler.js
+++ src/Handlers/EventoHandler.js
@@ -24,7 +24,23 @@ const getEventoHandler = async (req, res) => {
   }
 };
 
+const addEventoHandler = async  (req, res) =>{
+    const data = req.body;
+    try {
+        const result = await addEventoController(data);
+        return res.status(201).json(result);
+    } catch (error) {
+        return res.status(201).json({message: error.message});
+    }
+}
+
+const modEventoHandler = async ( req, res ) => {
+    
+}
+
 module.exports = {
     getEventoHandler,
     getEventoGridHandler,
+    addEventoHandler,
+    modEventoHandler
 }
