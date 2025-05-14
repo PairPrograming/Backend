672600c7f9997a8e9dafc574c85573fb35bdb8e2
Slasho01 <josuerendon01@gmail.com>
2025-05-13 20:06:38 -0400

    Resuelto error en obtener evento por id!!

--- src/Controllers/EventoController.js
+++ src/Controllers/EventoController.js
@@ -51,7 +51,14 @@ const getEventosGridController = errorHandler(async (filters = {}) => {
 
 const getEventoController = errorHandler(async (id) => {
   const result = await Eventos.findByPk(id, {
-    attributes: EVENT_ATTRIBUTES,
+    attributes: EVENT_ATTRIBUTES,/*
+    include: [
+      {
+        model: Salones,
+        through: { attributes: [] },
+        attributes: ["Id", "salonId", "capacidad", "nombre"], // Incluye el nombre del salón
+      },
+    ],*/
   });
 
   if (!result) {
