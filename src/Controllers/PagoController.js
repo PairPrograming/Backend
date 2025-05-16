52f735d5f7e4e5b641d74c9b8c43ede449d032f4
Slasho01 <josuerendon01@gmail.com>
2025-05-16 00:00:16 -0400

    pago en prueba, modificacion de rutas modelos y controllers de ordenes y pagos

--- src/Controllers/PagoController.js
+++ src/Controllers/PagoController.js
@@ -1,4 +1,4 @@
-const { Pago, MetodoDePago, Orden, conn } = require("../DbIndex");
+const { Pago, MetodoDePago, Orden, DetalleDeOrden, Entrada, conn } = require("../DbIndex");
 
 const crearPago = async (data) => {
   const { ordenId, metodoDeCobroId, estatus, referencia, descripcion } = data;
@@ -7,50 +7,86 @@ const crearPago = async (data) => {
   try {
     const metodoDePago = await MetodoDePago.findByPk(metodoDeCobroId);
     if (!metodoDePago) {
+      await t.rollback();
       return { success: false, message: "Método de cobro no encontrado" };
     }
     const { comision, impuesto } = metodoDePago;
 
-    const orden = await Orden.findByPk(ordenId);
+    const orden = await Orden.findByPk(ordenId, {
+        include: [{
+          model: DetalleDeOrden,
+          required: true,
+        }],
+        transaction: t,
+      });
+
     if (!orden) {
+      await t.rollback();
       return { success: false, message: "Orden no encontrada" };
     }
+
     const montoBase = parseFloat(orden.total);
     const comisionPorcentaje = parseFloat(comision);
     const impuestoPorcentaje = parseFloat(impuesto);
     const comisionMonto = montoBase * (comisionPorcentaje / 100);
-    const impuestoMonto =
-      (montoBase + comisionMonto) * (impuestoPorcentaje / 100);
+    const impuestoMonto = (montoBase + comisionMonto) * (impuestoPorcentaje / 100);
     const total = montoBase + comisionMonto + impuestoMonto;
+
     const [pago, creado] = await Pago.findOrCreate({
-        where: { ordenId },
-        defaults: {
-          monto: montoBase,
-          comision: comisionMonto,
-          impuestos: impuestoMonto,
-          total,
-          estatus,
-          fecha_pago: new Date(),
-          referencia,
-          descripcion,
-          ordenId,
-          metodoDeCobroId,
-        },
-        transaction: t,
-      });
-      orden.estado = "pagado";
-      orden.fecha_pago = new Date();
-      await orden.save({ transaction: t });
-      await t.commit();
-      if (!creado) {
+      where: { ordenId },
+      defaults: {
+        monto: montoBase,
+        comision: comisionMonto,
+        impuestos: impuestoMonto,
+        total,
+        estatus,
+        fecha_pago: new Date(),
+        referencia,
+        descripcion,
+        ordenId,
+        metodoDeCobroId,
+      },
+      transaction: t,
+    });
+
+    if (!creado) {
+      await t.rollback();
+      return {
+        success: false,
+        message: "El pago ya ha sido registrado previamente para esta orden.",
+      };
+    }
+
+
+    for (const detalle of orden.DetalleDeOrdens) {
+      const entrada = await Entrada.findByPk(detalle.entradaId, { transaction: t});
+      if (!entrada) {
+        await t.rollback();
         return {
           success: false,
-          message: "El pago ya ha sido registrado previamente para esta orden.",
+          message: `Entrada con ID ${detalle.entradaId} no encontrada.`,
         };
       }
-      return { success: true, pago };
+      if (entrada.cantidad < detalle.cantidad) {
+        await t.rollback();
+        return {
+          success: false,
+          message: `No hay suficientes entradas disponibles para la entrada ${detalle.entradaId}.`,
+        };
+      }
+      console.log(detalle.cantidad)
+      entrada.cantidad -= detalle.cantidad;
+      await entrada.save({ transaction: t });
+    }
+
+    orden.estado = "pagado";
+    orden.fecha_pago = new Date();
+    await orden.save({ transaction: t });
+
+    await t.commit();
+    return { success: true, pago };
   } catch (error) {
-    await t.rollback();
+    if (!t.finished) await t.rollback();
     return { success: false, message: error.message };
   }
 };
