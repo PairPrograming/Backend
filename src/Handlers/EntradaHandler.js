71a89a41de4edbe2ca22c1b04fc3c69c6be5d1e3
Pablito471 <pablohernanbrito@gmail.com>
2025-09-21 19:39:41 -0300

    implementacion de delete en subtipos

--- src/Handlers/EntradaHandler.js
+++ src/Handlers/EntradaHandler.js
@@ -3,11 +3,12 @@ const {
   obtenerEntradasController,
   deleteEntradaController,
   obtenerEntradaByIdController,
-  actualizarEntradaController, 
+  actualizarEntradaController,
   agregarSubtipoController,
   actualizarSubtipoController,
+  deleteSubtipoController,
 } = require("../Controllers/EntradaController");
-const { genericHandler } = require("../utils//Handler");
+const { genericHandler } = require("../utils/Handler");
 
 const agregarEntradasHandler = genericHandler(agregarEntradasController);
 const obtenerEntradasHandler = genericHandler((eventoId) =>
@@ -19,16 +20,20 @@ const obtenerEntradaByIdHandler = genericHandler((id) =>
 const deleteEntradashandler = genericHandler((id) =>
   deleteEntradaController(id)
 );
-const actualizarEntradaHandler = genericHandler(actualizarEntradaController); // Handler para PUT
+const actualizarEntradaHandler = genericHandler(actualizarEntradaController);
 const agregarSubtipoHandler = genericHandler(agregarSubtipoController);
 const actualizarSubtipoHandler = genericHandler(actualizarSubtipoController);
+const deleteSubtipoHandler = genericHandler((id) =>
+  deleteSubtipoController(id)
+);
 
 module.exports = {
   agregarEntradasHandler,
   obtenerEntradasHandler,
   obtenerEntradaByIdHandler,
   deleteEntradashandler,
-  actualizarEntradaHandler, // Exporta el nuevo handler
+  actualizarEntradaHandler,
   agregarSubtipoHandler,
   actualizarSubtipoHandler,
+  deleteSubtipoHandler,
 };
